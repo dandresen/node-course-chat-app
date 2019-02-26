f701ec3d911c310b867966ba09522f4ac0cf7d1d
dandresen <dandresen01@gmail.com>
2019-02-25 20:29:21 -0600

    Added geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate a message', () => {
@@ -12,5 +12,18 @@ describe('generateMessage', () => {
         expect(typeof message.createdAt).toBe('number');
         expect(message).toMatchObject({from,text});
 
+    });
+});
+
+describe('generageLocationMessage', () => {
+    it('should generate correct location ', () =>{
+        var from = 'Angie';
+        var lat = 30;
+        var long = 98;
+        var url = 'https://www.google.com/maps?q=30,98';
+        var message = generateLocationMessage(from, lat, long);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from, url});
     });
 });
